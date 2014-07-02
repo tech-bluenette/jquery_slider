7f1eb4693069e0ba65c4d684415ec38055734a26
tech-bluenette <lindseyandmina@gmail.com>
2014-07-01 23:02:23 -0700

    initial commit

---
+++ slider.js
@@ -0,0 +1,12 @@
+// Below code: "wait until page is done loading so that elements have time to load.""
+$(document).ready(function () {
+
+ $(function() {
+            $( "#slider-1" ).slider();
+         });
+
+});
+
+
+
+
